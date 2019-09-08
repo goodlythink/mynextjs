f5f8dab51df4c55edae7ce20e9e5f31221a6c305
goodlythink <goodlythink@gmail.com>
2019-09-08 11:41:49 +0700

    Link by Next.js

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default () =>(
+    <div>
+        <Link href="/"><a>Home</a></Link>
+        
+        <h2>About Page</h2>
+    </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const Index = () => (
+    <div>
+        <Link href="/about"><a>About</a></Link>
+        <h2>Hello Next.JS</h2>
+    </div>
+)
+
+export default Index
